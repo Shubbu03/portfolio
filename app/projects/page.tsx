2b69c8171a220673bad3271e9ce989aef1a363d9
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-13 00:12:33 +0530

    projects page init

---
+++ app/projects/page.tsx
@@ -0,0 +1,10 @@
+export default function Projects() {
+  return (
+    <>
+      <div className="w-full max-w-2xl px-4 pt-16">
+        <h1 className="text-3xl mb-8 text-start">my projects</h1>
+      </div>
+      <div className="flex-grow flex items-center justify-center"></div>
+    </>
+  );
+}
