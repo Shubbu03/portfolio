bbcd17e0c59f565981b868a8ec2991be4fd361d5
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-12 19:15:33 +0530

    refactor Home component to include Landing and Dock components; add profile picture

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Landing from "./components/Landing";
+import Dock from "./components/Dock";
+
 export default function Home() {
-  return <div>portfolio</div>;
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-black text-black dark:text-white font-mono transition-colors duration-300">
+      <Landing />
+      <Dock />
+    </main>
+  );
 }
