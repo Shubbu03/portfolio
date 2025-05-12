6b21bae367e30e8b6f2c14902ce19f28840e7475
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-13 00:13:28 +0530

    relocated components folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
-import Dock from "./components/Dock";
+import Dock from "../components/Dock";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
