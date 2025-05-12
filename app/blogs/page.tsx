225846135e56650668581072eb579688cbf3a0b5
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-05-12 23:44:10 +0530

    blogs page

---
+++ app/blogs/page.tsx
@@ -0,0 +1,13 @@
+export default function Blogs() {
+  return (
+    <main className="min-h-screen flex flex-col items-center bg-white dark:bg-black text-black dark:text-white font-mono transition-colors duration-300 select-none">
+      <div className="w-full max-w-2xl px-4 pt-16">
+        <h1 className="text-3xl mb-8 text-start">my blogs</h1>
+      </div>
+
+      <div className="flex-grow flex items-center justify-center">
+        <p className="text-lg text-center">some cool blogs soon...</p>
+      </div>
+    </main>
+  );
+}
